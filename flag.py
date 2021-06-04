4a9cf20012f2a5567730f05259bc31ea60caea01
Akash <choudharyaakash316@gmail.com>
2021-06-04 19:19:30 +0530

    Indian TriColor Flag

---
+++ flag.py
@@ -0,0 +1,24 @@
+import cv2
+import numpy as np
+#Saffron Color
+saffron = cv2.imread("saffron.jpg")
+saffron = saffron[0:135 , :]
+cv2.imwrite("saffron_color.jpg"  , saffron )
+
+#White Color
+white = np.zeros([135, 600, 3])
+white[:, :] =  [255, 255, 255]
+cv2.imwrite("white_color.jpg" , white)
+
+#Green Color
+green = np.zeros([135, 600, 3])
+green[:, :] =  [0, 255, 0]
+cv2.imwrite("green_color.jpg" , green)
+s = cv2.imread("saffron_color.jpg")
+w  = cv2.imread("chakr.jpg")
+g = cv2.imread("green_color.jpg")
+tricolor = np.vstack( (s, w, g) )
+# tricolor[142:258 , 200:400]=chakr[142:258 , 200:400]
+cv2.imshow("hi", tricolor)
+cv2.waitKey()
+cv2.destroyWindow()
